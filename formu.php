531e3feef74240d6d240c3810ed21ce96ef40dbd
mac <mac@iMac-de-mac.fibertel.com.ar>
2020-08-10 04:00:38 -0300

    commit php

---
+++ formu.php
@@ -0,0 +1,11 @@
+<?php
+$name =$_POST ['name'];
+$email =$_POST ['email'];
+$password =$_POST ['password'];
+
+if (!empty($nombre) || !empty($email) || !empty($password)){
+    $host = "localhost";
+    $dbusername =
+}
+
+?>
